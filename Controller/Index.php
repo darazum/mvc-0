fa33cf76ebfb0a1ad50d3db06098292f19ead106
darazum <razumovskiy@playform.com>
2019-08-04 14:12:01 +0300

    mvc

---
+++ Controller/Index.php
@@ -0,0 +1,22 @@
+<?php
+class Index
+{
+    /** @var View */
+    public $view;
+
+    function indexAction()
+    {
+        include "../modelUser.php";
+        $user = new modelUser();
+        $user->setName('Dima');
+
+    }
+
+    function userProfileAction()
+    {
+        include "../modelUser.php";
+        $user = new modelUser();
+        $user->load($_GET['id']);
+        $this->view->user = $user;
+    }
+}
